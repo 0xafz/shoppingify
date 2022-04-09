979b692cc150047ae52b5a665c88fb2b31d2c0cf
zkindest <zkindest5@gmail.com>
2022-04-09 19:05:15 +0530

    fix api/lists

--- src/pages/api/lists/index.ts
+++ src/pages/api/lists/index.ts
@@ -13,46 +13,61 @@ export default async function handle(
     const loggedUser = checkAuth(req)
 
     switch (method) {
-      case "GET": {
-        checkAuth(req)
+      case "GET":
+        {
+          checkAuth(req)
 
-        const { cursor, limit = 10 } = req.query
-        const lists = await prisma.shoppingList.findMany({
-          where: {
-            userId: loggedUser.id,
-          },
-          take: Number(limit),
-          orderBy: {
-            id: "asc",
-          },
-          cursor: {
-            id: Number(cursor),
-          },
-        })
-        res.status(200).json({
-          data: {
-            lists,
-            nextCursor: lists.length ? lists[lists.length - 1].id : "",
-          },
-        })
-      }
-      case "POST": {
-        const { name, status, items } = req.body
-
-        if (!name || !status) throw new ClientError("empty parameters")
-        const newList = await prisma.shoppingList.create({
-          data: {
-            name,
-            status,
-            items: {
-              create: Array.isArray(items) ? items : [],
+          const { cursor, limit = 10 } = req.query
+          const lists = await prisma.shoppingList.findMany({
+            where: {
+              userId: loggedUser.id,
             },
-            createdAt: new Date().toISOString(),
-            userId: loggedUser.id,
-          },
-        })
-        res.status(200).json({ data: newList })
-      }
+            take: Number(limit),
+            orderBy: {
+              id: "asc",
+            },
+            cursor: cursor
+              ? {
+                  id: Number(cursor),
+                }
+              : undefined,
+          })
+          res.status(200).json({
+            data: {
+              lists,
+              nextCursor: lists.length ? lists[lists.length - 1].id : "",
+            },
+          })
+        }
+        break
+      case "POST":
+        {
+          const { name, status = "incomplete", items: rawItems = [] } = req.body
+
+          const createdAt = new Date().toISOString()
+          const items = rawItems.map((item) => {
+            item.createdAt = createdAt
+          })
+
+          if (!name || !status) throw new ClientError("empty parameters")
+          const newList = await prisma.shoppingList.create({
+            data: {
+              name,
+              status,
+              items: {
+                create: items,
+              },
+              createdAt: new Date().toISOString(),
+              user: {
+                connect: {
+                  id: loggedUser.id,
+                },
+              },
+            },
+          })
+          res.status(200).json({ data: newList })
+        }
+        break
       default:
         res.setHeader("Allow", ["GET", "POST"])
         res.status(405).end(`Method ${method} Not Allowed`)
