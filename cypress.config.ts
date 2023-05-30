12252df12a8969d56f9debf99524be6c138992bf
0xafz <0xafz.dev@gmail.com>
2023-05-30 18:24:21 +0530

    feat: Reset Database state after each spec run and `deleteUserByEmail` cypress node task

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,3 +1,4 @@
+import prisma from './src/lib/prisma'
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
@@ -6,6 +7,31 @@ export default defineConfig({
     /**
      * defaults to medium screen, check ~/src/styles/base.css for more info 
      */
-    viewportWidth: 768
+    viewportWidth: 768,
+    setupNodeEvents(on, config) {
+      on('task', {
+        async deleteUserByEmail(email: string) {
+          try {
+            console.log(`info: Deleting User with email: ${email}`)
+          await prisma.user.delete({
+            where: {
+              email
+            }
+          })
+          } catch (error) {
+            console.error(`error: Something went wrong while deleting User with email: ${email}`)
+          }
+        },
+      }),
+        on('after:spec', async () => {
+          try {
+            console.log('info: Wiping User data...')
+            await prisma.user.deleteMany()
+            console.log('info: Wiped User data successfully.')
+          } catch (error) {
+            console.error('error: Something went wrong while wiping User data.')
+          }
+        })
+    },
   },
 })
